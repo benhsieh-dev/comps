1b5188dce746d5027eccd9e14b19aadc6c828eae
benhsieh-dev <bqh5026@gmail.com>
2021-03-05 14:22:12 -0500

    Assemble table in table component template using *ngFor logic

--- src/app/collections/collections-home/collections-home.component.ts
+++ src/app/collections/collections-home/collections-home.component.ts
@@ -1,26 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-collections-home',
-  templateUrl: './collections-home.component.html',
-  styleUrls: ['./collections-home.component.css']
+  selector: "app-collections-home",
+  templateUrl: "./collections-home.component.html",
+  styleUrls: ["./collections-home.component.css"],
 })
 export class CollectionsHomeComponent implements OnInit {
-
   data = [
-    { name: 'James', age: 24, job: 'Designer'},
-    { name: 'Jill', age: 26, job: 'Engineer'},
-    { name: 'Elyse', age: 25, job: 'Engineer'},
+    { name: "James", age: 24, job: "Designer", employed: true },
+    { name: "Jill", age: 26, job: "Engineer", employed: false},
+    { name: "Elyse", age: 25, job: "Engineer", employed: true },
   ];
   headers = [
-    { key: 'name', label: 'Name'},
-    { key: 'age', label: 'Age'},
-    { key: 'job', label: 'Job'},
-  ]
+    { key: "employed", label: "Employed" },
+    { key: "name", label: "Name" },
+    { key: "age", label: "Age" },
+    { key: "job", label: "Job" },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
