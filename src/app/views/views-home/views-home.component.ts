c745b6cf04cd4d8c5ab55452dacadd29c853705a
benhsieh-dev <bqh5026@gmail.com>
2021-05-26 23:43:23 -0400

    Add item list component to display new images

--- src/app/views/views-home/views-home.component.ts
+++ src/app/views/views-home/views-home.component.ts
@@ -1,21 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-views-home',
-  templateUrl: './views-home.component.html',
-  styleUrls: ['./views-home.component.css']
+  selector: "app-views-home",
+  templateUrl: "./views-home.component.html",
+  styleUrls: ["./views-home.component.css"],
 })
 export class ViewsHomeComponent implements OnInit {
   stats = [
-    { value: 22, label: '# of users'},
-    { value: 900, label: 'Revenue'},
-    { value: 50, label: 'Reviews'},
-  ]
-  
+    { value: 22, label: "# of users" },
+    { value: 900, label: "Revenue" },
+    { value: 50, label: "Reviews" },
+  ];
 
-  constructor() { }
+  items = [
+    { image: "/assets/images/2-tier-heart-shaped-cake.jpg", title: '2 Tier Heart Shaped Cake', description: "Engagement Cake"},
+    { image: "/assets/images/wine-cake.jpg", title: 'Wine Cake', description: 'Party Cake'}
+  ];
 
-  ngOnInit(): void {
-  }
+  constructor() {}
 
+  ngOnInit(): void {}
 }
