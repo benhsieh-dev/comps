4b2fac23881996bbc93b24747e9aef6d63f70f4c
benhsieh-dev <bqh5026@gmail.com>
2021-06-03 14:58:39 -0400

    Add close modal function

--- src/app/mods/modal/modal.component.ts
+++ src/app/mods/modal/modal.component.ts
@@ -1,22 +1,26 @@
-import { ElementRef } from '@angular/core';
+import { ElementRef, EventEmitter, Output } from '@angular/core';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-modal',
-  templateUrl: './modal.component.html',
-  styleUrls: ['./modal.component.css']
+  selector: "app-modal",
+  templateUrl: "./modal.component.html",
+  styleUrls: ["./modal.component.css"],
 })
 export class ModalComponent implements OnInit {
-
+  @Output() close = new EventEmitter();
+  
   constructor(private el: ElementRef) {}
 
   ngOnInit(): void {
-    document.body.appendChild(this.el.nativeElement); 
+    document.body.appendChild(this.el.nativeElement);
   }
 
   ngOnDestroy() {
-    console.log('Leving Modal!');
+    console.log("Leving Modal!");
     this.el.nativeElement.remove();
   }
 
+  onCloseClick() {
+    this.close.emit(); 
+  }
 }
